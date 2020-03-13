83758748dec8cd9916e9975ee34deaaf49e9fd57
NiamhMcCartney <nmccartney06@qub.ac.uk>
2020-03-13 16:22:17 +0000

    Added ScoreBoard class

---
+++ app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/ScoreBoard.java
@@ -0,0 +1,31 @@
+package uk.ac.qub.eeecs.game.cardDemo;
+
+import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
+import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.world.GameScreen;
+import uk.ac.qub.eeecs.gage.world.LayerViewport;
+import uk.ac.qub.eeecs.gage.world.ScreenViewport;
+import uk.ac.qub.eeecs.gage.world.Sprite;
+
+public class ScoreBoard extends Sprite {
+
+    public ScoreBoard(GameScreen screen){
+        super(0.0f, 0.0f, 0.0f, 0.0f, null, screen);
+    }
+
+    /**
+     * Draw the ScoreBoard Object
+     *
+     * @param elapsedTime    Elapsed time information
+     * @param graphics2D     Graphics instance
+     * @param layerViewport  Game layer viewport
+     * @param screenViewport Screen viewport
+     *
+     * Created by Niamh McCartney
+     */
+    @Override
+    public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D,
+                     LayerViewport layerViewport, ScreenViewport screenViewport) {
+
+    }
+}
