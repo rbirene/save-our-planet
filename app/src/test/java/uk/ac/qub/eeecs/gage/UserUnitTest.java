89c6562ef04c8450cb46d381e3d47652dd073fc1
NiamhMcCartney <nmccartney06@qub.ac.uk>
2020-03-24 17:41:18 +0000

    Added User Tests

---
+++ app/src/test/java/uk/ac/qub/eeecs/gage/UserUnitTest.java
@@ -0,0 +1,96 @@
+package uk.ac.qub.eeecs.gage;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import uk.ac.qub.eeecs.game.cardDemo.User.User;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserUnitTest {
+
+    private User aUser;
+    private int numOfWins = 4;
+    private int numOfLosses = 6;
+
+    @Before
+    public void SetUp() {
+        aUser = new User("David", numOfWins, numOfLosses);
+    }
+
+    @Test
+    public void getName_CorrectName_Success(){
+        assertEquals("David", aUser.getName());
+    }
+
+    @Test
+    public void getWins_CorrectNumber_Success(){
+        assertEquals(4, aUser.getWins());
+    }
+
+    @Test
+    public void getLosses_CorrectNumber_Success(){
+        assertEquals(6, aUser.getLosses());
+    }
+
+    @Test
+    public void calculateGamesPlayed_ReturnsWinsPlusLosses_Success(){
+        assertEquals(10, aUser.getGamesPlayed());
+    }
+
+    @Test
+    public void setName_CorrectName_Success(){
+        String newName = "Sean";
+        aUser.setName(newName);
+        assertEquals(newName, aUser.getName());
+    }
+
+    @Test
+    public void setWins_CorrectNumber_Success(){
+        int wins = 8;
+        aUser.setWins(wins);
+        assertEquals(wins, aUser.getWins());
+    }
+
+    @Test
+    public void setLosses_CorrectNumber_Success(){
+        int losses = 7;
+        aUser.setLosses(losses);
+        assertEquals(losses, aUser.getLosses());
+    }
+
+    @Test
+    public void setUser_CorrectProperties_Success(){
+        String name = "Claire";
+        int wins = 2;
+        int losses = 3;
+        User newUser = new User(name, wins , losses);
+        aUser.setUser(newUser);
+        Boolean correctProperties = aUser.getName() == name && aUser.getWins() == 2 && aUser.getLosses() == 3;
+        assertTrue(correctProperties);
+    }
+
+    @Test
+    public void getWinRateRatio_CorrectNumberCalculated_Success(){
+        assertEquals(0.4, aUser.getWinRateRatio(), 0.05);
+    }
+
+    @Test
+    public void addWin_WinsIncrease_Success(){
+        aUser.addWin();
+        int newNumOfWins = numOfWins + 1;
+        assertEquals(newNumOfWins, aUser.getWins());
+    }
+
+    @Test
+    public void addLosses_LossesIncrease_Success(){
+        aUser.addLoss();
+        int newNumOfLosses = numOfLosses + 1;
+        assertEquals(newNumOfLosses, aUser.getLosses());
+    }
+
+}
